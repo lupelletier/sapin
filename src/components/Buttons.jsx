e25c74ae33067363cf4811d19b197a067a376c57
luludegex06 <95019526+Luludegex06@users.noreply.github.com>
2022-12-20 17:39:28 +0100

    first commit

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,81 @@
+import {useState} from 'react';
+
+
+export default function Buttons ( props ) {
+    
+    const [state, setState] = useState({ setState: false });
+    const [isLoading, setIsLoading] = useState(false);
+    const [err, setErr] = useState('');
+
+    const handleClick = async () => {
+
+        setIsLoading(true);
+        try {
+        const response = await fetch('https://sphere-relay.azurewebsites.net/api/Mxchip1Control?code=Vt4WZR7G7Oit3augAdmMR9HH87u8QELjambh4GNuEdfWoC6jfA5NLQ==&SetSwitch=On&DeviceId=mxchip1', {
+            method: 'GET',
+            headers: {
+            Accept: 'application/json',
+            },
+        });
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+
+        console.log('result is: ', JSON.stringify(result, null, 4));
+
+        setState(result);
+        } catch (err) {
+        setErr(err.message);
+        } finally {
+        setIsLoading(false);
+        }
+        console.log(state);
+    };
+
+
+    const handleClickOff = async () => {
+
+        setIsLoading(true);
+        try {
+        const response = await fetch('https://sphere-relay.azurewebsites.net/api/Mxchip1Control?code=Vt4WZR7G7Oit3augAdmMR9HH87u8QELjambh4GNuEdfWoC6jfA5NLQ==&SetSwitch=Off&DeviceId=mxchip1', {
+            method: 'GET',
+            headers: {
+            Accept: 'application/json',
+            },
+        });
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+
+        console.log('result is: ', JSON.stringify(result, null, 4));
+
+        setState(result);
+        } catch (err) {
+        setErr(err.message);
+        } finally {
+        setIsLoading(false);
+        }
+        console.log(state);
+    };
+
+
+
+
+
+    
+    return (
+        <div>
+            {err && <h2>{err}</h2>}
+            {isLoading && <h2>Loading...</h2>}
+            <h1>The christmas tree is </h1>
+            <button onClick={handleClick} SetSwitch='on'>Turn On</button>
+            <button onClick={handleClickOff}  SetSwitch='off'>Turn Off</button>
+        </div>
+    );
+}
+
+
